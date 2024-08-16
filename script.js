a649be20a29cfe8d6987b1152dcb01ba552fdc9d
MorganaMiranda <168208762+MorganaMiranda@users.noreply.github.com>
2024-08-16 15:02:29 -0300

    hfhgkjfgjkbfjkgj

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
 
+]
 
 
